d7c285a14194bd4c415ceddf3255838c80ce7fe4
liaohuancheng <949104781@qq.com>
2019-03-30 17:17:54 +0800

    添加141和142

---
+++ LeetCode/No_141.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class No_141
+    {
+        public bool HasCycle(ListNode head)
+        {
+            var p1 = head;
+            var p2 = head;
+            while (true)
+            {
+                if (p2 == null || p2.next == null)
+                    return false;
+                p2 = p2.next.next;
+                p1 = p1.next;
+                if (p1 == p2)
+                    return true;
+            }
+        }
+    }
+}
