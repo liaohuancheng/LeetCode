417a5b7b0296fca48480e381e883557029426b37
liaohuancheng <949104781@qq.com>
2019-03-30 17:08:23 +0800

    111

---
+++ LeetCode/No_142.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class No_142
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            if (head == null)
+                return null;
+            ListNode p1 = head, p2 = head, p3 = head;
+            while (p1.next != null && p2.next != null && p2.next.next != null)
+            {
+                p1 = p1.next;
+                p2 = p2.next.next;
+                if (p1 == p2)
+                {
+                    while (p1 != p3)
+                    {
+                        p1 = p1.next;
+                        p3 = p3.next;
+
+                    }
+                    return p1;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ LeetCode/No_160.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class No_160
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            ListNode p1, p2;
+            if (headA == null || headB == null) return null;
+            p1 = headA;
+            p2 = headB;
+            while (p1 != p2)
+            {
+                if (p1 == null)
+                    p1 = headB;
+                else
+                    p1 = p1.next;
+                if (p2 == null)
+                    p2 = headA;
+                else
+                    p2 = p2.next;
+            }
+            return p1;
+    }
+}
